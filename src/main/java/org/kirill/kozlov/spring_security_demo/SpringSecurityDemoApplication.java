8f1b40dd6a730b59df4e3cf84194fd49dd82ebfe
Kirill Kozlov <space_cell@proton.me>
2024-06-13 20:37:56 +0500

    Init commit

---
+++ src/main/java/org/kirill/kozlov/spring_security_demo/SpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package org.kirill.kozlov.spring_security_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+
+}
